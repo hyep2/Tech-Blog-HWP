b8e3a2b3e62f62c0ef40e42775df744218f4468a
Hye Won Park <hyewonpark26@gmail.com>
2022-04-15 00:18:11 -0700

    set up modularized folders

---
+++ server.js
@@ -0,0 +1,29 @@
+require("dotenv").config;
+
+const session = require("express-session");
+const express = require("express");
+const { join } = require("path");
+
+const app = express();
+
+//MIDDLEWARE
+app.use(express.static(join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const sess = {
+  secret: "Super secret secret",
+  resave: false,
+  saveUninitialized: true,
+};
+
+app.use(session(sess));
+
+app.use(require("./routes"));
+
+async function init() {
+  await require("./config/config.js").sync();
+  app.listen(process.env.PORT || 3001);
+}
+
+init();
