d67e16b4513a02d9f277305990677b1a87df0a35
Hye Won Park <hyewonpark26@gmail.com>
2022-04-18 11:24:55 -0700

    user model keys input;still need to do pw hashing

--- models/User.js
+++ models/User.js
@@ -6,7 +6,23 @@ class User extends Model { }
 
 User.init(
   {
-
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [6]
+      }
+    }
   },
   {
     sequelize,
