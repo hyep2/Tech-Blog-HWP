6f6bb10a893f941d4e2d460dd11f1874fb6a38f9
Hye Won Park <hyewonpark26@gmail.com>
2022-04-25 20:41:31 -0700

    user route register

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,60 +2,22 @@ const router = require ('express').Router()
 const { User } = require('../models')
 const passport = require('passport')
 const jwt = require('jsonwebtoken')
-// const bcrypt = require('bcrypt')
+
 
 
 //registering route
-router.post('/register', async({body,session},res)=> {
-  try {
-    const newUser = await User.create( {
-      username: body.username,
-      password: body.password
-    })
-
-  
-    session.save( () => {
-      session.id = newUser.id
-      session.username = newUser.username
-      session.loggedIn = true
-
-      res.json(newUser)
-    })
-
-  } catch (error) {
-    res.json({error})
-  }
+router.post('/register', (req, res)=> {
+  const { username } = req.body
+  User.register(new User({username}), req.body.password, err=> {
+    if(err) {console.log(err)}
+    res.json({message: 'User has been registered'})
+  })
 })
 
 //logging in route
-router.post('/login', async({body}, res) => {
-  try {
-    //search db for user with the provided email
-    const dbUser = await User.findOne({where: {email: body.email}})
 
-    //if provided email doesn't match
-    if (!dbUser) {
-      res.json({message: 'Login failed. Please try again'})
-      return
-    }
 
-    //checking password
-
-    //comparing provided pw with the hashed pw
-    const validPassword = await bcrypt.compare(body.password, dbUser.password)
-
-    //if provided pw doesnt match
-    if(!validPassword) {
-      res.json({message: 'Login failed. Please try again'})
-    }
-
-    //if they match:
-    res.json({message: 'You are now logged'})
-
-  } catch (error) {
-    res.json({error})
-  }
-})
+//will return a jsonwebtoken after inputting login info, which can be used to recognize who the user is and give access to what we want to look at; can restrict it to users only
 
 
 //logging out route
